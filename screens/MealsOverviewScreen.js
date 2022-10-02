ccc5128500c908d53cb17ece282999d51c458fe3
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-10-02 22:37:33 +0200

    displaying meals in the pressed category

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
+import { View, FlatList } from "react-native";
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 
 export default function MealsOverview({ route }) {
-    const catId = route.params.categoryId
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title}></MealItem>
+  };
+
   return (
     <View>
-      <Text>Meals Overview screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
