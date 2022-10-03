e768f36dbfe9c7a9fb4b615f748ac8f6c67323ee
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-10-03 10:04:20 +0200

    styling the meals item component

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,4 +1,11 @@
-import { Text, View, Pressable, Image, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  Pressable,
+  Image,
+  StyleSheet,
+  Platform,
+} from "react-native";
 
 export default function MealItem({
   title,
@@ -8,16 +15,23 @@ export default function MealItem({
   affordability,
 }) {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Text style={styles.title}>{title}</Text>
-          <Image source={{ uri: imageUrl }} style={styles.image} />
-        </View>
-        <View>
-          <Text>{duration}m</Text>
-          <Text>{complexity.toUpperCase()}m</Text>
-          <Text>{affordability.toUpperCase()}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) =>
+          pressed ? { opacity: 0.5 } : { opacity: null }
+        }
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image source={{ uri: imageUrl }} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{duration}m</Text>
+            <Text style={styles.detailItem}>{complexity.toUpperCase()}m</Text>
+            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+          </View>
         </View>
       </Pressable>
     </View>
@@ -25,6 +39,21 @@ export default function MealItem({
 }
 
 const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
   image: {
     width: "100%",
     height: 200,
@@ -33,5 +62,16 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
   },
 });
