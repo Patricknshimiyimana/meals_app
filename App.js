b046ded2ede4761ef30a3a8450f3bd2f8840a9ee
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-10-05 21:20:56 +0200

    nesting navigators - bottom tabs & stack

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverview from "./screens/MealsOverviewScreen";
 import MealDetailsScreen from "./screens/MealDetailsScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import { BlurView } from "expo-blur";
+import { StyleSheet } from "react-native";
 
 const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+function BottomTabNavigator() {
+  return (
+    <BottomTab.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#39414E" },
+        headerTintColor: "white",
+        backgroundColor: "#24180f",
+        sceneContainerStyle: { backgroundColor: "#6C7D87" },
+        tabBarStyle: {
+          // backgroundColor: 'black',
+      },
+      }}
+    >
+      <BottomTab.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="menu" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -22,9 +64,9 @@ export default function App() {
         >
           <Stack.Screen
             name="MealsContainer"
-            component={CategoriesScreen}
+            component={BottomTabNavigator}
             options={{
-              title: "All Categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverview} />
